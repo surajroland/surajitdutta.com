24ad2e06c8708669c4fab59d558eea59b497560c
Surajit Dutta <suraj.roland@gmail.com>
2017-10-08 06:03:07 +0200

    project refactored

--- js/agency.js
+++ js/agency.js
@@ -35,3 +35,23 @@ $('div.modal').on('show.bs.modal', function() {
 		}
 	}
 });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('poster');
+var modalImg = document.getElementById("poster01");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
